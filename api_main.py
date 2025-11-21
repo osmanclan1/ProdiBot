84d37337a9092ab3d228990ce0125d5184290a0a
Yb <you@example.com>
2025-11-21 13:27:02 -0600

    error handling

--- api_main.py
+++ api_main.py
@@ -73,7 +73,7 @@ if not all([DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, SECRET_KEY]):
 
 # --- Config ---
 API_BASE_URL = "http://3.83.248.40:8000"
-FRONTEND_URL = "d1wdxkpmgii4om.cloudfront.net"
+FRONTEND_URL = "https://d1wdxkpmgii4om.cloudfront.net/"
 
 DISCORD_AUTH_URL = "https://discord.com/oauth2/authorize"
 DISCORD_TOKEN_URL = "https://discord.com/api/oauth2/token"
